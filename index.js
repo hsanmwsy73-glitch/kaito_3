32943113493349a88c0d5eed6dadec903b272a64
hsanmwsy73-glitch <hsanmwsy73@gmail.com>
2026-06-02 10:20:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import sub from './sub.js';
 
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber: '963934149274', // Bot number
+  phoneNumber: 201093036797', // Bot number
   prefix: [".", "/", "!"],
   fromMe: false, 
   owners: [
